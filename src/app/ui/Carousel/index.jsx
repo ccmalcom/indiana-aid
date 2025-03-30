872d2d6db02537c23a050c6e3c607633d52045c9
ccmalcom <chasecmalcom@gmail.com>
2025-03-30 17:09:56 -0400

    update to about page, uploaded sample artwork

--- src/app/ui/Carousel/index.jsx
+++ src/app/ui/Carousel/index.jsx
@@ -3,20 +3,28 @@ import React, { useEffect } from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
 
-export default function Carousel() {
+export default function Carousel({images}) {
     const [emblaRef] = useEmblaCarousel({ loop: true
     }, [Autoplay()]);
+    console.log(JSON.stringify(images));
 
-
+    if (!images || images.length === 0) {
+        return <div>No images available</div>;
+    }
 
   
     return (
-      <div className="embla" ref={emblaRef}>
-        <div className="embla__container">
-          <div className="embla__slide">Slide 1</div>
-          <div className="embla__slide">Slide 2</div>
-          <div className="embla__slide">Slide 3</div>
+        <div className="w-[80vw] mx-auto flex flex-col h-[40vh] ">
+            <div className="overflow-hidden embla" ref={emblaRef}>
+                <div className="embla__container">
+                    {/* Map through the images and create a slide for each */}
+                    {images.map((image, index) => (
+                        <div key={index} className="embla__slide">
+                            <img src={image.src} alt={`Slide ${index + 1}`} />
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
-      </div>
     )
 }
