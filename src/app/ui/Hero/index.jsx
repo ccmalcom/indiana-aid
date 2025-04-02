65e9ebb990cb491a27c9803ddf853b99b50dd8bb
ccmalcom <chasecmalcom@gmail.com>
2025-04-02 16:54:58 -0400

    heading size update

--- src/app/ui/Hero/index.jsx
+++ src/app/ui/Hero/index.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
         <div className="hero relative grid grid-cols-3 w-[80vw] h-[40vh] md:h-[400] mx-auto my-8 bg-cover bg-center">
             <div className="absolute bottom-0 right-0 w-[33%] h-full bg-yellow "></div>
             <div id='left-hero' className="relative z-10 col-span-2 flex flex-col items-center justify-center text-center md:mx-20">
-                <p className="text-2xl text-white ">Providing Advocacy, Resources, and Direct Aid to Immigrants and their families impacted by ICE detention in Indiana.</p>
+                <p className="text-3xl text-white ">Providing Advocacy, Resources, and Direct Aid to Immigrants and their families impacted by ICE detention in Indiana.</p>
             </div>
             <div id='right-hero' className="relative z-10 col-span-1 flex flex-col items-center justify-center">
                 <a href="/donate">
