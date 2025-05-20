d2799a8b76a0d19a667bd2c4f9616787158cfb10
ccmalcom <chasecmalcom@gmail.com>
2025-05-20 15:42:15 -0400

    formatting on hero

--- src/app/ui/Hero/index.jsx
+++ src/app/ui/Hero/index.jsx
@@ -3,10 +3,10 @@ import Link from 'next/link';
 export default function Hero() {
 	return (
 		<div className="hero relative grid grid-cols-3 w-[80vw] h-[40vh] md:h-[400] mx-auto my-8 bg-cover bg-center">
-			<div className="absolute bottom-0 right-0 w-[33%] h-full bg-yellow "></div>
+			{/* <div className="absolute bottom-0 right-0 w-[33%] h-full bg-yellow "></div> */}
 			<div
 				id="left-hero"
-				className="relative z-10 col-span-2 flex flex-col items-center justify-center text-center md:mx-20">
+				className="relative col-span-2 flex flex-col items-center justify-center text-center md:mx-20 pl-10">
 				<p className="text-3xl text-white font-heading">
 					Providing resources, direct aid, and friendship to immigrants and their
 					families impacted by ICE detention in Indiana.
@@ -14,7 +14,7 @@ export default function Hero() {
 			</div>
 			<div
 				id="right-hero"
-				className="relative z-10 col-span-1 flex flex-col items-center justify-center">
+				className="relative col-span-1 flex flex-col items-center justify-center pr-10">
 				<Link href="/donate">
 					<button className="bg-blue hover:bg-blue-700 text-white font-heading py-2 px-4 my-4 rounded">
 						Donate
