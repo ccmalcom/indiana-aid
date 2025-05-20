abd10f4312ff29df01b38cbb971e1fdba2fd6e17
ccmalcom <chasecmalcom@gmail.com>
2025-05-20 19:14:33 -0400

    more font change (to open sans)

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,5 @@
 import localFont from 'next/font/local';
-import { Inter, Urbanist } from 'next/font/google';
+import { Inter, Urbanist, Open_Sans } from 'next/font/google';
 import './globals.css';
 import Nav from './ui/Nav';
 import Footer from './ui/Footer';
@@ -12,7 +12,7 @@ config.autoAddCss = false;
 const urbanist = Urbanist({ subsets: ['latin'], variable: '--font-urbanist', display: 'swap' });
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter', display: 'swap' });
-
+const openSans = Open_Sans({ subsets: ['latin'], variable: '--font-open-sans', display: 'swap' }); 
 export const metadata = {
 	title: 'Indiana AID',
 	description: 'Providing Advocacy, Resources, and Direct Aid',
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body
-				className={`${urbanist.variable} antialiased min-h-[100vh] flex flex-col`}>
+				className={`${openSans.variable} antialiased min-h-[100vh] flex flex-col`}>
 				<Nav />
 				<main className="flex-grow">{children}</main>
 				<Footer />
