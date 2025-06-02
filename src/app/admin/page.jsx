f0f43745acf5ed3c7116479a56d3545d9087e0fd
ccmalcom <chasecmalcom@gmail.com>
2025-06-02 18:12:01 -0400

    middleware seems to be doing nothing

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -10,6 +10,16 @@ import EmailListCard from './ui/Cards/EmailListCard';
 export default async function Admin() {
 
     const user = await getUserDetails();
+    if (!user) {
+        return (
+            <div className="flex flex-col items-center justify-center h-screen">
+                <h1 className="text-2xl font-bold mb-4">You must be logged in to access the admin dashboard.</h1>
+                <Link href="/auth/login" className="text-blue-500 hover:underline">
+                    Log in
+                </Link>
+            </div>
+        );
+    }
     const applications = await getPendingVolunteerApplications();
     const newsletters = await getNewsletterInfo();
     const subscriberCount = await getNewsletterSubscriberCount();
--- src/app/utils/middleware.js
+++ src/app/utils/middleware.js
@@ -1,16 +1,11 @@
-import { NextRequest } from 'next/server';
 import { updateSession } from './supabase/middleware';
 
 export async function middleware(request) {
-  // Ensure the request is a NextRequest instance
-  if (!(request instanceof NextRequest)) {
-    throw new Error('Middleware must be used with NextRequest');
-  }
   return await updateSession(request);
 }
 
 export const config = {
   matcher: [
-    '/admin/*)',
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 };
