2e018fa9c6c93b17dc65961ff06b5949e0f0909f
ccmalcom <chasecmalcom@gmail.com>
2025-07-02 14:39:41 -0400

    more mobile nav fixes/styling

--- src/app/ui/Nav/NavMenu.jsx
+++ src/app/ui/Nav/NavMenu.jsx
@@ -4,12 +4,18 @@ import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { useState } from 'react';
 
-export default function NavMenu({isMenuOpen, setIsMenuOpen}) {
+export default function NavMenu({ isMenuOpen, setIsMenuOpen }) {
 	const currentRoute = usePathname();
 	const [showDropdown, setShowDropdown] = useState(false);
 
 	return (
-    <div className={`pr-6 text-xl ${isMenuOpen ? 'flex flex-col bg-blue absolute top-full right-0 w-full text-right' : 'hidden'} md:block`}>
+						
+		<div
+			className={`${
+				isMenuOpen
+					? 'fixed top-0 right-0 w-32 h-full z-50 bg-blue text-white flex flex-col pt-24 space-y-6 transition-transform transform translate-x-0'
+					: 'hidden'
+			} text-xl md:block md:relative md:top-0 md:right-0 md:flex md:flex-row md:items-center md:space-x-6 md:pt-0`}>
 			<Link
 				onClick={() => setIsMenuOpen(false)}
 				href="/"
@@ -35,13 +41,13 @@ export default function NavMenu({isMenuOpen, setIsMenuOpen}) {
 				Donate
 			</Link>
 			<div
-				className="relative inline-block"
+				className="relative inline-block p-4"
 				onMouseEnter={() => setShowDropdown(true)}
 				onMouseLeave={() => setShowDropdown(false)}>
 				<Link
 					onClick={() => setIsMenuOpen(false)}
 					href="/resources"
-					className={`p-4 ${currentRoute.startsWith('/resources') ? 'text-yellow' : 'hover:text-yellow'}`}>
+					className={` ${currentRoute.startsWith('/resources') ? 'text-yellow' : 'hover:text-yellow'}`}>
 					Resources
 				</Link>
 
@@ -69,19 +75,20 @@ export default function NavMenu({isMenuOpen, setIsMenuOpen}) {
 				Contact
 			</Link>
 			{/* translate dropdown */}
-			<select
-				className="ml-4 bg-white text-blue px-2 py-1 rounded text-sm"
-				defaultValue="en"
-				onChange={(e) => {
-					const selectedLang = e.target.value;
-					// You can replace this with your language-switching logic
-					alert(`Switching to: ${selectedLang}`);
-				}}>
-				<option value="en">English</option>
-				<option value="es">Español</option>
-				<option value="ar">العربية</option>
-				<option value="fr">Français</option>
-			</select>
+			<div className="w-full flex p-4 md:p-0 md:pr-4 md:mt-0 md:justify-end">
+				<select
+					className="bg-white text-blue px-2 py-1 rounded text-sm"
+					defaultValue="en"
+					onChange={(e) => {
+						const selectedLang = e.target.value;
+						alert(`Switching to: ${selectedLang}`);
+					}}>
+					<option value="en">English</option>
+					<option value="es">Español</option>
+					<option value="ar">العربية</option>
+					<option value="fr">Français</option>
+				</select>
+			</div>
 		</div>
 	);
 }
--- src/app/ui/Nav/index.jsx
+++ src/app/ui/Nav/index.jsx
@@ -11,30 +11,50 @@
 import Image from 'next/image';
 import NavMenu from './NavMenu';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-export default  function Nav() {
-
+export default function Nav() {
 	const [isOpen, setIsOpen] = useState(false);
+	useEffect(() => {
+		if (isOpen) {
+			document.body.style.overflow = 'hidden';
+		} else {
+			document.body.style.overflow = 'auto';
+		}
+
+		const handleResize = () => {
+			if (window.innerWidth >= 768) {
+				setIsOpen(false);
+			}
+		};
+		window.addEventListener('resize', handleResize);
+	}, [isOpen]);
+
 
 
 	return (
 		<nav
 			className={`sticky top-0 z-50 bg-blue text-white text-xl flex justify-between items-center h-20 transition-shadow shadow-lg`}
-			role="navigation"
-		>
+			role="navigation">
 			<Link href="/" className="ml-4">
-			<Image
-				src="/logos/horizontal/Logo_white.png"
-				alt="logo"
-				width={200}
-				height={200}
-				className="ml-12"
-			/>
+				<Image
+					src="/logos/horizontal/Logo_white.png"
+					alt="logo"
+					width={200}
+					height={200}
+					className="ml-12"
+				/>
 			</Link>
-			<div className="px-4 cursor-pointer pr-8 md:hidden" onClick={()=>setIsOpen(!isOpen)}>Menu</div>
-			<NavMenu isMenuOpen={isOpen} setIsMenuOpen={setIsOpen}/>
-			
+			<div
+				className="px-4 cursor-pointer pr-8 md:hidden"
+				onClick={() => setIsOpen(!isOpen)}>
+				<div className="space-y-1">
+					<span className="block w-6 h-0.5 bg-white"></span>
+					<span className="block w-6 h-0.5 bg-white"></span>
+					<span className="block w-6 h-0.5 bg-white"></span>
+				</div>
+			</div>
+			<NavMenu isMenuOpen={isOpen} setIsMenuOpen={setIsOpen} />
 		</nav>
 	);
 }
