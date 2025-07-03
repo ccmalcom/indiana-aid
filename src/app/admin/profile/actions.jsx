53b94550703b8d5114e405793a821196da4d7c55
ccmalcom <chasecmalcom@gmail.com>
2025-07-03 15:30:33 -0400

    change baseUrl getting

--- src/app/admin/profile/actions.jsx
+++ src/app/admin/profile/actions.jsx
@@ -5,8 +5,8 @@ import { createClient } from '@/app/utils/supabase/server';
 export async function sendPasswordReset(email, pathname) {
     console.log(`Preparing to reset password for email: ${email}`);
 	const supabase = await createClient();
-    console.log(`Supabase client created: ${supabase}`);
-    const redirectUrl = 'https://localhost:3000'+ pathname + '/update-password';
+    const baseUrl = process.env.NEXT_PUBLIC_SITE_URL;
+    const redirectUrl = `${baseUrl}${pathname}/update-password`;
     console.log(`Redirecting to: ${redirectUrl}`);
 	const { error } = await supabase.auth.resetPasswordForEmail(email, {
 		redirectTo: redirectUrl,
