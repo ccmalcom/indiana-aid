63b51d672afe2d7210a684f0b87116ada57d4536
ccmalcom <chasecmalcom@gmail.com>
2025-07-14 17:39:18 -0400

    fix cookie setting

--- src/app/api/set-language/route.js
+++ src/app/api/set-language/route.js
@@ -1,18 +1,22 @@
-import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
 
 export async function POST(req) {
-    const cookieStore = await cookies();
 	const { language } = await req.json();
 
 	if (!language) {
 		return new Response('Missing language', { status: 400 });
 	}
 
-	cookieStore.set('language', language, {
+	console.log('Saving language to cookies:', language);
+
+	const res = NextResponse.json({ message: 'Language set' });
+
+	res.cookies.set('language', language, {
 		path: '/',
 		httpOnly: false,
+		sameSite: 'lax',
 		maxAge: 60 * 60 * 24 * 30, // 30 days
 	});
 
-	return new Response('Language set', { status: 200 });
+	return res;
 }
