e49ba1496b5b07c0d26793c306d9a116ee1b6969
ccmalcom <chasecmalcom@gmail.com>
2025-07-31 13:59:53 -0400

    clean up

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,11 +1,8 @@
-// 'use client';
 
 import ProfileCard from './ui/Cards/ProfileCard';
 import VolunteerCard from './ui/Cards/VolunteerCard';
-import { getUserDetails, getVolunteerApplications, getNewsletterCardInfo, getNewsletterSubscriberCount, getMailingListSubscribers } from './actions';
+import { getUserDetails, getVolunteerApplications, getNewsletterCardInfo, getNewsletterSubscriberCount } from './actions';
 import NewsletterCard from './ui/Cards/NewsletterCard';
-import EmailListCard from './ui/Cards/EmailListCard';
-//next/navigation is used for client-side navigation
 import { redirect } from 'next/navigation';
 
 export default async function Admin() {
@@ -16,16 +13,14 @@ export default async function Admin() {
     const applications = await getVolunteerApplications();
     const newsletters = await getNewsletterCardInfo();
     const subscriberCount = await getNewsletterSubscriberCount();
-    const subscribers = await getMailingListSubscribers();
    
   return (
     <div className="p-6 bg-white shadow rounded-lg">
       <h1 className="text-3xl font-bold mb-6 text-center">Admin Dashboard</h1>
       <div className="flex flex-row align-items-center justify-evenly">
-        <ProfileCard user={user} />
         <VolunteerCard applications={applications}/>
         <NewsletterCard newsletters={newsletters} subscriberCount={subscriberCount} />
-        {/* <EmailListCard subscribers={subscribers} /> */}
+        <ProfileCard user={user} />
       </div>
     </div>
   );
