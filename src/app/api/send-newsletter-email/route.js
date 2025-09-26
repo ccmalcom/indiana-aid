0802ad5211332fe06d6a9d78a82aa7132457f0bf
ccmalcom <chasecmalcom@gmail.com>
2025-09-26 14:06:35 -0400

    todo and formatting

--- src/app/api/send-newsletter-email/route.js
+++ src/app/api/send-newsletter-email/route.js
@@ -4,19 +4,19 @@ import NewsletterEmailTemplate from "@/app/components/newsletter-email-template"
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req) {
-	const { issue, message, mensaje } = await req.json();
+    const { issue, message, mensaje } = await req.json();
+    // todo: add attachments (spanish pdf, english pdf from newsletters storage bucket)
+    try {
+        const email = await resend.emails.send({
+            from: "Indiana AID <info@indianaaid.org>",
+            to: "chasecmalcom@gmail.com",
+            subject: `Indiana AID Newsletter #${issue} & Resources / Boletín Informativo #${issue} y Recursos`,
+            react: NewsletterEmailTemplate({ issue, message, mensaje }),
+        });
 
-    try{
-	const email = await resend.emails.send({
-		from: "Indiana AID <info@indianaaid.org>",
-		to: "chasecmalcom@gmail.com",
-		subject: `Indiana AID Newsletter #${issue} & Resources / Boletín Informativo #${issue} y Recursos`,
-		react: NewsletterEmailTemplate({ issue, message, mensaje }),
-	});
-
-	return new Response(JSON.stringify(email), { status: 200 });
-} catch (error) {
-	console.error('Error sending email:', error);
-	return new Response('Error sending email', { status: 500 });        
+        return new Response(JSON.stringify(email), { status: 200 });
+    } catch (error) {
+        console.error('Error sending email:', error);
+        return new Response('Error sending email', { status: 500 });
     }
 }
